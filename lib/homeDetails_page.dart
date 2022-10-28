4c339388ca22c9609fee97bbfe212bfb85dc4f9a
MrHarsh007 <porwalharsh007@gmail.com>
2022-10-28 17:46:27 +0530

    Add Drawer and load pdf in cach memory

--- lib/homeDetails_page.dart
+++ lib/homeDetails_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cached_pdfview/flutter_cached_pdfview.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 import 'package:velocity_x/velocity_x.dart';
 import '../models/catelog.dart';
@@ -15,19 +16,26 @@ class HomeDetailsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          foregroundColor: Colors.black,
-          title: "${catelog.name}".text.bold.xl2.make(),
-        ),
-        body: (catelog.pdf.isEmpty)
-            ? Center(child: "No Data Avalaible".text.bold.xl4.makeCentered())
-            : SfPdfViewer.network(
-                catelog.pdf,
-                // enableDoubleTapZooming: true,
-                pageLayoutMode: PdfPageLayoutMode.continuous,
-              ),
-      ),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            foregroundColor: Colors.black,
+            title: "${catelog.name}".text.bold.xl2.make(),
+          ),
+          body: (catelog.pdf.isEmpty)
+              ? Center(child: "No Data Avalaible".text.bold.xl4.makeCentered())
+              : PDF()
+                  .cachedFromUrl(
+                    catelog.pdf,
+
+                    maxAgeCacheObject: Duration(days: 1),
+                    //duration of cache
+
+                    placeholder: (progress) =>
+                        Center(child: Text('$progress %')),
+                    errorWidget: (error) =>
+                        Center(child: Text(error.toString())),
+                  )
+                  .scale100()),
     );
   }
 }
